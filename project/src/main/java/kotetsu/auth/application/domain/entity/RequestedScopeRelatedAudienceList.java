da9ad8a651e81f1cce24c22baf6ca37b54290938
nakano <83113782+nakano2024@users.noreply.github.com>
2025-08-16 20:20:10 +0900

    refactor: ユースケースの完成

--- project/src/main/java/kotetsu/auth/application/domain/entity/RequestedScopeRelatedAudienceList.java
+++ project/src/main/java/kotetsu/auth/application/domain/entity/RequestedScopeRelatedAudienceList.java
@@ -21,8 +21,14 @@ public class RequestedScopeRelatedAudienceList {
         this.audiences = audiences;
     }
 
-    public static RequestedScopeRelatedAudienceList of(List<AccessTokenAudience> audiences) {
-        RequestedScopeRelatedAudienceList requestedScopeRelatedAudienceList = new RequestedScopeRelatedAudienceList(
+    public static RequestedScopeRelatedAudienceList of(final List<String> audienceNameStrings) {
+        // openidスコープなどaudienceに紐づかないスコープが含まれている場合nullとなるため、フィルタリング
+        final List<AccessTokenAudience> audiences = audienceNameStrings.stream()
+            .filter(audienceNameString -> audienceNameString != null)
+            .map(audienceNameString -> AccessTokenAudience.of(audienceNameString))
+            .collect(Collectors.toList());
+
+        final RequestedScopeRelatedAudienceList requestedScopeRelatedAudienceList = new RequestedScopeRelatedAudienceList(
             new LinkedHashSet<>(audiences)
         );
 
--- project/src/main/java/kotetsu/auth/repository/ExistingAccessTokenCoreRepository.java
+++ project/src/main/java/kotetsu/auth/repository/ExistingAccessTokenCoreRepository.java
@@ -1,15 +1,70 @@
 package kotetsu.auth.repository;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 
 import kotetsu.auth.application.domain.entity.ExistingAccessTokenCore;
+import kotetsu.auth.application.domain.entity.RequestedScopeList;
+import kotetsu.auth.application.domain.entity.RequestedScopeRelatedAudienceList;
+import kotetsu.auth.application.domain.entity.Scope;
 import kotetsu.auth.application.domain.repository.IFetchExistingAccessTokenCorePort;
+import kotetsu.auth.application.domain.value.Issuer;
 import kotetsu.auth.application.domain.value.Key;
+import kotetsu.auth.application.domain.value.ScopeName;
+import kotetsu.auth.application.domain.value.Subject;
 
 public class ExistingAccessTokenCoreRepository implements IFetchExistingAccessTokenCorePort {
+    private final NamedParameterJdbcOperations template;
+
+    public ExistingAccessTokenCoreRepository(final NamedParameterJdbcOperations template) {
+        this.template = template;
+    }
     
     @Override
     public Optional<ExistingAccessTokenCore> fetch(Key key) {
-        return Optional.empty();
+        final String sql = """
+            SELECT atc.key, atc.issuer, atc.subject, s.key as s_key, s.name as s_name, rs.url as rs_url 
+            FROM access_token_cores as atc
+            JOIN access_token_core_scopes as atcs ON atc.key = atcs.access_token_core_key
+            JOIN scopes as s ON atcs.scope_key = s.key
+            LEFT JOIN scope_audiences as sa ON s.key = sa.scope_key
+            LEFT JOIN resource_servers as rs ON sa.resource_server_key = rs.key
+            WHERE atc.key = :key;
+        """;
+
+        final Map<String, Object> params = new HashMap<>();
+        params.put("key", key.getValue());
+
+        final List<Map<String, Object>> rows = template.queryForList(sql, params);
+
+        if (rows.isEmpty()) {
+            return Optional.empty();
+        }
+
+        final List<Scope> scopes = rows.stream()
+            .map(row -> Scope.of(
+                Key.of((String) row.get("s_key")),
+                ScopeName.of((String) row.get("s_name")))
+            )
+            .collect(Collectors.toList());
+        final RequestedScopeList requestedScopeList = RequestedScopeList.of(scopes);
+
+        final List<String> resourceServerUrls = rows.stream()
+            .map(row -> (String) row.get("rs_url"))
+            .collect(Collectors.toList());
+        final RequestedScopeRelatedAudienceList scopeRelatedAudienceList = RequestedScopeRelatedAudienceList.of(resourceServerUrls);
+
+        return Optional.of(ExistingAccessTokenCore.of(
+            Key.of((String) rows.get(0).get("key")),
+            Issuer.of((String) rows.get(0).get("issuer")),
+            Subject.of((String) rows.get(0).get("subject")),
+            requestedScopeList,
+            scopeRelatedAudienceList
+        ));
     }
 }
