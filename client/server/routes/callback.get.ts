d524b09cae990eda6a7c2579b92d7f75a91843d4
nakano <nakano7771@ymail.ne.jp>
2025-11-19 01:40:49 +0900

    feat: トークンリクエストの仮実装

--- client/server/routes/callback.get.ts
+++ client/server/routes/callback.get.ts
@@ -1,30 +1,32 @@
 
 import { defineEventHandler, type H3Event, getQuery } from 'h3'
 import { CallbackParameter, OAuth2Parameter, OAuthFetchTokenResponse, OidcParameter } from '~/types/auth';
+import { FetchError } from 'ofetch'
 
 export default defineEventHandler(async (event: H3Event): Promise<void> => {
-    const callbackParameter = getQuery<Partial<CallbackParameter>>(event);
-
-    const ERROR_MESSAGE_400 = '不正なリクエストです。' ;
-    if (!callbackParameter.code || !callbackParameter.state) {
-        throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
-    }
-
-    const { oauth2Parameter,  oidcParameter } = await getUserSession(event);
-
-    const oauth2SessionParameter = oauth2Parameter as OAuth2Parameter;
-    const oidcSessionParameter = oidcParameter as OidcParameter;
-    if (!oauth2SessionParameter?.state || !oauth2SessionParameter?.codeVerifier || !oidcSessionParameter?.nonce) {
-        throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
-    }
-
-    if (callbackParameter.state !== oauth2SessionParameter.state) {
-        throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
-    }
-
-    const config = useRuntimeConfig();
-    const basicAuthCredential = Buffer.from(`${config.clientId}:${config.clientSecret}`, 'utf8').toString('base64');
     try {
+        const callbackParameter = getQuery<Partial<CallbackParameter>>(event);
+
+        const ERROR_MESSAGE_400 = '不正なリクエストです。' ;
+        if (!callbackParameter.code || !callbackParameter.state) {
+            throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
+        }
+
+        const { oauth2Parameter,  oidcParameter } = await getUserSession(event);
+
+        const oauth2SessionParameter = oauth2Parameter as OAuth2Parameter;
+        const oidcSessionParameter = oidcParameter as OidcParameter;
+        if (!oauth2SessionParameter?.state || !oauth2SessionParameter?.codeVerifier || !oidcSessionParameter?.nonce) {
+            throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
+        }
+
+        if (callbackParameter.state !== oauth2SessionParameter.state) {
+            throw createError({ statusCode: 400, message: ERROR_MESSAGE_400, fatal: false });
+        }
+
+        const config = useRuntimeConfig();
+        const basicAuthCredential = Buffer.from(`${config.clientId}:${config.clientSecret}`, 'utf8').toString('base64');
+
         const tokenResponse = await $fetch<OAuthFetchTokenResponse>(`${config.idpUrl}/api/oauth2/token`, {
             method: 'POST',
             headers: {
@@ -41,6 +43,18 @@ export default defineEventHandler(async (event: H3Event): Promise<void> => {
     }
     catch(error: any) {
         await clearUserSession(event);
+        if (error?.name === 'FetchError') {
+            const status = error.status;
+            const apiCode = error.data?.code;
+            console.error('FetchError status:', status);
+            console.error('API error code:', apiCode);
+
+            throw createError({
+              statusCode: status,
+              message: "トークンの取得に失敗しました。",
+              data: { code: apiCode }
+            });
+        }        
         throw error;
     }
 });
